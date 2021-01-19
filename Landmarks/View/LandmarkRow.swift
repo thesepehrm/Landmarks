86aac35e49caa4c0a0227848d2944a478034b333
Sepehr Mohammadi <mohamadi.sepehr@live.com>
2021-01-19 15:27:26 +0330

    implemented landmark row

---
+++ Landmarks/View/LandmarkRow.swift
@@ -0,0 +1,36 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Sepehr Mohammadi on 1/19/21.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50, alignment: .center)
+            Text(landmark.name).font(.title3)
+            
+            Spacer()
+            Image(systemName: "chevron.right").foregroundColor(.gray)
+        }.padding()
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+                .previewLayout(.fixed(width: 300, height: 70))
+            LandmarkRow(landmark: landmarks[1])
+                .previewLayout(.fixed(width: 300, height: 70))
+        }
+        
+    }
+}
