5495ae9d9a479978ba515152be2f94776b5efcc2
Sepehr Mohammadi <mohamadi.sepehr@live.com>
2021-01-20 22:40:37 +0330

    moved list to detail view

---
+++ Landmarks/View/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Sepehr Mohammadi on 1/18/21.
+//
+
+import SwiftUI
+import UIKit
+
+struct ContentView: View {
+
+    var body: some View {
+        LandmarkList()
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
